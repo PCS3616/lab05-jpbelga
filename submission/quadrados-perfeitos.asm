0c27f6da66b646facfc8ad272e9c587e9a2b1232
João Pedro Belga <jpedrobelga@usp.br>
2025-02-21 23:44:45 -0300

    6

--- submission/quadrados-perfeitos.asm
+++ submission/quadrados-perfeitos.asm
@@ -4,21 +4,16 @@ BEGIN	LV /2
 	
 	AD INSTR ; Altera a INSTR para adicionar nos lugares corretos
 	MM INSTR
-
 	LD SQR
 	AD INT
-	
 INSTR	K /9100 ; Save on instr INSTR + 2
-	
 	MM SQR
 	LV /2
 	AD INT
 	MM INT
-	
 	LV =126
 	SB INT
 	MM INT
-	
 	JN END
 	JP BEGIN
 END	HM /0000
